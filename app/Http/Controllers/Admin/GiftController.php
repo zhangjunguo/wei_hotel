b14894c3b08e68877cf94b9bb03337e86b911ac0
shenningting <1244426046@qq.com>
2016-06-20 14:42:00 +0800

    申

---
+++ app/Http/Controllers/Admin/GiftController.php
@@ -0,0 +1,133 @@
+<?php 
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use DB,Request,Session;
+
+class GiftController extends Controller
+{		
+	/**
+	 * 礼物列表
+	 */
+	public function giftList()
+    {
+        $gift_data = DB::table('gift') ->paginate(3);
+       // print_r($gift_data);
+        return view('admin/giftList',['gift_data'=>$gift_data]);
+    }
+
+    /**
+     * 加载礼物添加页面
+     */
+    public function giftAdd()
+    {
+        return view('admin/giftAdd');
+    }
+
+    /**
+     * 礼物添加
+     */
+    public function addGift()
+    {
+        $file = Request::file('g_img');
+        $entension = $file->getClientOriginalExtension(); //上传文件的后缀.
+        $newName = time() . "." . $entension;
+        $path = $file->move('storage/uploads/', $newName);
+        if($path){
+            $gift_data['g_name'] = Request::input('g_name');
+            $gift_data['g_score'] = Request::input('g_score');
+            $gift_data['g_text'] = Request::input('g_text');
+            $gift_data['g_num'] = Request::input('g_num');
+            $gift_data['g_img'] = $newName;
+            $bool = DB::table('gift') -> insert($gift_data);
+            if($bool){
+                return redirect('giftList');
+            }
+        }
+    }
+
+    /**
+     * 礼物验证唯一性
+     */
+    public function uniqueGift()
+    {
+
+        $g_name = Request::input('g_name');
+        $bool = DB::table('gift')
+            ->where('g_name',$g_name)
+            ->first();
+        if($bool){
+            echo 1;
+        }else{
+            echo 0;
+        }
+    }
+
+    /**
+     * 礼物删除
+     */
+    public function giftDel()
+    {
+        // $g_id = $_GET['g_id'];
+        $g_id = Request::input('g_id');
+        $bool = DB::table('gift')
+            ->where('g_id',$g_id)
+            ->delete();
+        if($bool){
+            return redirect('giftList');
+        }
+    }
+
+    /**
+     * 加载礼物编辑页面
+     */
+    public function giftEdit()
+    {
+       // $g_id = $_GET['g_id'];
+        $g_id = Request::input('g_id');
+        $gift_data = DB::table('gift')
+            ->where('g_id',$g_id)
+            ->first();
+        return view('admin/giftEdit',['gift_data'=>$gift_data]);
+    }
+
+    /**
+     * 礼物添加
+     */
+    public function editGift()
+    {
+        $g_id = Request::input('g_id');
+        $file = Request::file('g_img');
+        if($file==''){
+            $gift_data['g_name'] = Request::input('g_name');
+            $gift_data['g_score'] = Request::input('g_score');
+            $gift_data['g_text'] = Request::input('g_text');
+            $gift_data['g_num'] = Request::input('g_num');
+            $bool = DB::table('gift')
+                ->where('g_id',$g_id)
+                -> update($gift_data);
+            if($bool){
+                return redirect('giftList');
+            }
+
+        }else{
+            $entension = $file->getClientOriginalExtension(); //上传文件的后缀.
+            $newName = time() . "." . $entension;
+            $path = $file->move('storage/uploads/', $newName);
+            if($path){
+                $gift_data['g_name'] = Request::input('g_name');
+                $gift_data['g_score'] = Request::input('g_score');
+                $gift_data['g_text'] = Request::input('g_text');
+                $gift_data['g_num'] = Request::input('g_num');
+                $gift_data['g_img'] = $newName;
+                $bool = DB::table('gift')
+                    ->where('g_id',$g_id)
+                    -> update($gift_data);
+                if($bool){
+                    return redirect('giftList');
+                }
+            }
+        }
+    }
+}
--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -1,54 +1,114 @@
-<?php 
-
+<?php
 namespace App\Http\Controllers\Admin;
-
+//header('content-type:text/html;charset=utf8');
 use DB,Request,Session;
+use Mail;
 use App\Http\Controllers\Controller;
-
 class LoginController extends Controller {
 
-     // 登录页面
-     public function login()
-     {
-          return view('admin.login');
-     }
- 
-     // 登录操作
-     public function PostLogin()
-     {    
-              $ip=Request::getClientIp();
-          $username=Request::input('username');
+    // 登录页面
+    public function login()
+    {
+        return view('admin.login');
+    }
 
-          $pwd=md5(Request::input('pwd'));
-          $user= DB::table('admin')->where('adm_name',$username)->where('adm_pass',$pwd)->first();
+    // 登录操作
+    public function PostLogin()
+    {
+        $adm_time = time();
+        $username = Request::input('username');
+        $pwd = md5(Request::input('pwd'));
+        $user= DB::table('admin')->where('adm_name',$username)->where('adm_pass',$pwd)->first();
+        // print_r($user);die;
+        if ($user){
+            if($adm_time - $user->adm_time>10){
+                DB::table('admin') -> where('adm_name',$username) -> update(['adm_update_num'=>0]);
+                Session::put('username',$username);
+                Session::put('uid',$user->adm_id);
+                return redirect('admin')->with('message', '成功登录');
+            }else{
+                echo '<script>alert("客官再等会,10秒不够呢");location.href="login"</script>';
+            }
+        }
+        else {
+            $bool = DB::table('admin')->where('adm_name',$username)->first();
+            if($bool){
+                //echo "<script>alert('ddd');location.href='login'</script>";
+                $adm_update_num = DB::table('admin') -> where('adm_name',$username) -> pluck('adm_update_num');
+                $adm_update_num = $adm_update_num+1;
+                if($adm_update_num >= 3){
+                    $adm_time = time();
+                    DB::table('admin') -> where('adm_name',$username) -> update(['adm_time'=>$adm_time]);
+                    echo '<script>alert("输入次数已够,请您10秒后再登");location.href="login"</script>';
+                }else{
+                    DB::table('admin') -> where('adm_name',$username) -> update(['adm_update_num'=>$adm_update_num]);
+                    echo '<script>alert("密码错误,输入次数还剩"+'.(3-$adm_update_num).');location.href="login"</script>';
+                }
 
-          // $pwd=Request::input('pwd');
-          // $user= DB::table('admin')->where('adm_name',$username)->where('adm_pass',md5($pwd))->first();;
+            }else{
+                echo "<script>alert('用户名错误');location.href='login'</script>";
+            }
+        }
+    }
 
-          // print_r($user);die;
-          if ($user){
-            Session::put('username',$username);
-            Session::put('ip',$ip);
-            Session::put('uid',$user->adm_id);
-            $uid=Session::get('uid');
-            $arr=DB::table('admin')
-            ->join('adm_role', 'adm_role.adm_id', '=', 'admin.adm_id')
-            ->join('role', 'adm_role.ro_id', '=', 'role.ro_id')
-            ->where('admin.adm_id',$uid)
-            ->first();
-            Session::put('rname',$arr->ro_name);
-            return redirect('admin')->with('message', '成功登录');
-          } 
-          else {
-            return redirect('login')->with('message', '用户名密码不正确') ->withInput();
-          }
-     }
- 
-     // 登出
-     public function Logout()
-     {
-          Session::forget('username');
-           return redirect('/login');
-     }
- 
- }
+    /*
+     *
+     * 忘记密码
+     */
+    public function forgotPass()
+    {
+        $email = Request::input('email');
+        //echo $email;die;
+        $url = 'http://www.hotel.com/resetPass?'.md5('email')."=".urlencode($email);
+        $bool =  Mail::send('admin.email', ['data' => $url], function($message)
+        {
+            $email = Request::input('email');
+            $message -> to($email, 'John Smith') -> subject('Welcome!');
+        });
+
+        if($bool){
+            echo "<script>alert('发送成功');location.href='https://mail.qq.com/'</script>";
+        }
+        /* Mail::raw('这是一封测试邮件', function ($message) {
+             $to = '1244426046@qq.com';
+             $message ->to($to)->subject('测试邮件');
+         });*/
+    }
+
+    /*
+     *
+     * 重置密码页面
+     */
+    public function resetPass()
+    {
+        $email = $_GET[md5('email')];
+        return view('admin.resetPass',['email'=>$email]);
+    }
+
+    /*
+     *
+     * 重置密码
+     */
+    public function passReset()
+    {
+        $adm_pass = Request::input('adm_pass');
+        $check_adm_pass = Request::input('check_adm_pass');
+       // echo $adm_pass;
+        //echo $check_adm_pass;
+        if($adm_pass==$check_adm_pass){
+            $email = Request::input('email');
+            $bool = DB::table('admin')->where('adm_email',$email)->update(['adm_pass'=>md5($adm_pass)]);
+            if($bool){
+                echo "<script>alert('修改成功');location.href='login'</script>";
+            }
+        }
+    }
+
+    // 登出
+    public function Logout()
+    {
+        Session::forget('username');
+        return redirect('/login');
+    }
+
+}
